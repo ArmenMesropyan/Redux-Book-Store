ead9cb051aad30a64bd396d596f608bd0825dda3
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-27 14:30:03 +0400

    Some styles changed and test data updated

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const BookListItem = ({ book }) => {
-    const { title, author } = book;
+    const { title, author, image } = book;
     return (
         <li className="books__item card mb-3 col-md-4">
             <h3 className="books__title card-header">
-                {`${author} - ${title}`}
+                <span className="books__title books__title--author text-success">
+                    { author }
+                </span>
+                <span className="books__title books__title--title">
+                    { title }
+                </span>
             </h3>
-            <img src="#" alt={title} className="books__image"/>
-            <div className="books__description card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
+            <img src={image} alt={title} className="books__image"/>
             <div className="books__actions card-body">
-                <button type="button" className="btn btn-primary">Add to cart</button>
+                <button type="button" className="btn btn-primary btn-lg btn-block">Add to cart</button>
             </div>
         </li>
     )
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -3,8 +3,9 @@ export default class BookstoreService {
         return [
             {
                 id: 1,
-                title: 'JavaScript Book',
-                author: 'Armen Mesropyan',
+                title: 'A Typographical Journey of Typography Through the Inland Printer, 1883-1900 compiled by Maurice Annenberg',
+                author: 'Inland Printer',
+                image: 'https://cdn.shopify.com/s/files/1/0880/2454/products/ATypographicalJourney_295x.jpg?v=1594920381',
             }
         ];
     }
